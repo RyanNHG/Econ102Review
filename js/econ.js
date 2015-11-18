c1eb8a236d146fb85063ea45c2b224772fc81fa6
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2015-11-17 18:27:46 -0600

    Add question panel, with sample input;

--- js/econ.js
+++ js/econ.js
@@ -8,11 +8,52 @@ var econ = (function(){
   var _init = function() {
     console.log('Econ init');
     nav.init();
+    _loadProblem(
+      'What bear is best?',
+      [
+        {text: 'Black bear', isCorrect: true},
+        {text: 'Polar bear', isCorrect: false},
+        {text: 'Grizzly bear', isCorrect: false},
+        {text: 'Panda bear', isCorrect: false},
+      ]
+    );
+  };
+
+  var _loadProblem = function(question, answers) {
+    var page = $('#page');
+
+    var html = '<div class="panel">'+
+      '<h3 class="question">'+question+'</h3>'+
+      '<ul class="answers">';
+
+      for(var i = 0; i < answers.length; i++)
+      {
+        var answer = answers[i];
+        var classes = (answer.isCorrect) ? ' correct' : '';
+        html += '<li class="answer'+classes+'">'+answer.text+'</li>';
+      }
+
+      html += '</ul>'+
+      '<div class="buttons">'+
+        '<button class="back">Back</button>'+
+        '<button class="next">Next</button>'+
+      '</div>'+
+    '</div>';
+
+    page.html(html);
+
+    $('.answer').click(_onAnswerClicked);
+  };
+
+  var _onAnswerClicked = function(){
+    $('.answer').removeClass('active');
+    $(this).addClass('active');
   };
 
   // Public members
   return {
-    init: _init
+    init: _init,
+    loadProblem: _loadProblem
   };
 
 })();
